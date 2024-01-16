052c784f70c9264c9a48ddc0b0b11e7cdbd6d1b9
Amanda Hiser <ahiser@somalogic.com>
2024-01-16 14:49:47 -0800

    Initial commit

---
+++ R/somatemplate-package.R
@@ -0,0 +1,13 @@
+#' @title
+#' somatemplate
+#'
+#' @description
+#' A template for pkgdown websites developed by SomaLogic Operating Co., Inc.
+#'
+#'
+#' @md
+#' @name somatemplate
+#' @docType package
+#' @author Amanda Hiser
+#' @keywords internal
+"_PACKAGE"
